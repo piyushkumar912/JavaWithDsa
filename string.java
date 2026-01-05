9b03d86dc4072de70ac34d6cda4e59d50a94f544
piyushkumar912 <piyushrana7766@gmail.com>
2026-01-06 02:19:06 +0530

    add file

---
+++ string.java
@@ -0,0 +1,25 @@
+
+public class string {
+    public static void main(String[] args) {
+       String m = "Hello World";
+        System.out.println(m);  
+         
+        //length finding
+         String s = "piysh Rana";
+         int n = s.length();
+         System.out.println(n);
+
+        //MORGAN STYNLEY
+        String sc= "Morgan Stynley";
+        int nx =sc.length();
+        String ans = sc.substring(1,nx-1);
+        char c1 = sc.charAt(0);
+        char c2 = sc.charAt(nx-1);
+        System.out.println(c2+ans+c1);
+
+        //
+
+        }
+     }
+
+    
