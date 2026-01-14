da84098f1ebd227f1bb31b74f51ea0c0a4c058df
piyushkumar912 <piyushrana7766@gmail.com>
2026-01-15 00:00:41 +0530

    add loop1.java with basic counting logic

---
+++ loop1.java
@@ -0,0 +1,11 @@
+public class loop1 {
+    public static void main (String[] args){
+        int n = 10;
+        int i = 1;
+    while( i<=n){
+        System.out.println(i);
+        i = i + 1;
+    }
+}
+    }
+
