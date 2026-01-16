f21d0ff5c5712db36581f79f601723d9de383a95
piyushkumar912 <piyushrana7766@gmail.com>
2026-01-16 23:21:23 +0530

    try pushing it with the terminal

--- loop1.java
+++ loop1.java
@@ -1,5 +1,6 @@
 public class loop1 {
     public static void main (String[] args){
+        System.out.println("My name is piyush khan ");
         int n = 10;
         int i = 1;
     while( i<=n){
