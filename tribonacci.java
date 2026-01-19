7a88b3dce39477f2e8ed4b89348e49f2ef276d82
piyushkumar912 <piyushrana7766@gmail.com>
2026-01-19 23:53:04 +0530

    123 baby

--- tribonacci.java
+++ tribonacci.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class tribonacci {
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
+        System.out.println("Hello baby");
         int n = sc.nextInt();
         if(n==0 || n==1){
             System.out.println(n);
