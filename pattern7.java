9c689b3197a7cbabcb95ce1c3b13efcb1c4fc047
opsomewhere <omprakash908987@gmail.com>
2026-02-08 23:49:40 +0530

    add file

---
+++ pattern7.java
@@ -0,0 +1,19 @@
+//(Q)--> Pattern 90 degree triangle
+
+
+
+
+
+import java.util.*;
+public class pattern7 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
