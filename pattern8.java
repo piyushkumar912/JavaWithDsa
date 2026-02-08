d7ce70fd3c9a5bbb75320fea53c6cf00204aaa60
piyushkumar912 <piyushranna7766@gmail.com>
2026-02-09 00:04:58 +0530

    add file

---
+++ pattern8.java
@@ -0,0 +1,18 @@
+//(Q)-->Print Triangle
+
+import java.util.*;
+public class pattern8 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){ 
+            for(int j = 1; j<=n-i5;j++){ //Space
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
