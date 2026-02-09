68a4ed9f7391ec9aa45575b097442f71141ad229
piyushkumar912 <piyushrana7766@gmail.com>
2026-02-09 23:28:52 +0530

    new changes in ll and add search algo

--- pattern8.java
+++ pattern8.java
@@ -6,13 +6,14 @@ public class pattern8 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 1; i<=n; i++){ 
-            for(int j = 1; j<=n-i5;j++){ //Space
+            for(int j = 1; j<=n-i;j++){ //Space
                 System.out.print(" ");
             }
             for(int j = 1; j<=2*i-1;j++){
                 System.out.print("*");
             }
             System.out.println();
+
         }
     }
 }
