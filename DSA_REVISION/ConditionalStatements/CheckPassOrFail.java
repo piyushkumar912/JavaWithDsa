6e6c6e844faa4739b8b763be9e815a1c0cf03970
piyushkumar912 <piyushrana7766@gmail.com>
2026-09-05 01:37:47 +0530

    Add Calculator, CheckPassOrFail, and SwitchStatements classes with basic functionality

---
+++ DSA_REVISION/ConditionalStatements/CheckPassOrFail.java
@@ -0,0 +1,11 @@
+//By the help of unary operator... 
+
+import java.util.*;
+public class CheckPassOrFail {
+    public static void main(String args[]){
+        int marks = 75;
+
+        String ReportCard = marks >= 35 ? "PASS" : "FAIL";
+        System.out.println(ReportCard);
+    }
+}
---
+++ DSA_REVISION/ConditionalStatements/SwitchStatements.java
@@ -0,0 +1,27 @@
+public class SwitchStatements {
+    public static void main(String args[]){
+
+//using Integer...
+        //  int number = 2;
+        //  switch(number){
+        //     case 1 : System.out.println("Samosa");
+        //              break;
+        //     case 2 : System.out.println("Burger");
+        //              break;
+        //     case 3 : System.out.println("Fruti");
+        //              break;
+        //     default : System.out.println("We wake up");
+
+//Using Character...
+        char ch = 'c';
+         switch(ch){
+            case 'a' : System.out.println("Samosa");
+                     break;
+            case 'b' : System.out.println("Burger");
+                     break;
+            case 'c' : System.out.println("Fruti");
+                     break;
+            default : System.out.println("We wake up");
+         }
+    }
+}
