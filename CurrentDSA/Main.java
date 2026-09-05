dea35494e145b10625f4c0cd883fcd83f50e4843
piyushkumar912 <piyushrana7766@gmail.com>
2026-09-05 23:38:02 +0530

    Loop file

--- CurrentDSA/Main.java
+++ CurrentDSA/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String []args){
         System.out.println("piyush");
-        System.out.println("Hello world");
+        System.out.println("Hello World");
         
     }
 }
---
+++ DSA_REVISION/Loop/While.java
@@ -0,0 +1,13 @@
+// Print Hello World 100th Times..
+import java.util.*;
+public class While {
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+    }
+}
